3416e3f9076e87bc3ef20c0dcfb287b293494e05
williansviana <williansrviana@gmail.com>
2024-05-31 18:02:35 -0300

    previsao de tempo

---
+++ script.js
@@ -0,0 +1,34 @@
+const  key = "7519cf5004defffb544055eb8eaeea90"
+
+function colocarDadosNaTela(dados) {
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".tempo").innerHTML = Math.floor(dados.main.temp)+"°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".icone"). src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    
+    
+    
+
+    console.log(dados);
+
+}
+
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then (resposta => resposta.json())
+
+    colocarDadosNaTela(dados);
+    
+}
+
+
+function cliqueiNoBotao() {
+
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade);
+        
+    
+}
